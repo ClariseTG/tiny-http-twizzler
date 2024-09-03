65d39d850b7d25f533a2ccd3f82de81685a99151
ananyabalagere <abalagere@gmail.com>
2024-09-03 11:08:40 -0700

    pushing additions. big mess

--- src/shim.rs
+++ src/shim.rs
@@ -27,54 +27,54 @@ impl<'a> SmolTcpListener<'a> {
     // each_addr() taken from the standard tcp implementation in Rust 
     // found here: https://doc.rust-lang.org/src/std/net/mod.rs.html
 
-    // fn each_addr<A: ToSocketAddrs, F, T>(addr: A, mut f: F, mut S: &SmolTcpListener<'a>) -> Result<T, ListenError>
-    // where
-    //     F: FnMut(IpListenEndpoint) -> Result<(), ListenError>,
-    // {
-    //     let addrs = match addr.to_socket_addrs() {
-    //         Ok(addrs) => addrs,
-    //         Err(e) => return Err(e),
-    //     };
-    //     let mut last_err = None;
-    //     for addr in addrs {
-    //         match f(addr) {
-    //             Ok(l) => {
-    //                 // populate the local_addr of struct
-    //                 S.local_addr = addr;
-    //                 return Ok(l)
-    //             },
-    //             Err(e) => last_err = Some(e),
-    //         }
-    //     }
-    //     Err(last_err.unwrap_or_else(|| {
-    //         ListenError::Unaddressable // is this right? lol
-    //     }))
-    // }
-    // pub fn bind(addr: &dyn ToSocketAddrs<Iter=SocketAddr>) -> SmolTcpListener<'a> {
-    //   /*
-    //   passed to bind: 
-    //   "127.0.0.1:0"
-    //   SocketAddr::from(([127, 0, 0, 1], 443))
-    //   let addrs = [ SocketAddr::from(([127, 0, 0, 1], 80)),  SocketAddr::from(([127, 0, 0, 1], 443)), ];
-    //   */
-    //   // trial socket
-    //   // probably to change the buffers!!!!
-    //   let rx_buffer = SocketBuffer::new(vec![0; 64]);
-    //   let tx_buffer = SocketBuffer::new(vec![0; 64]);
-    //   let mut sock = Socket::new(rx_buffer, tx_buffer);
-    //   let mut stcp_listener = SmolTcpListener{local_addr: None, socket: sock};
-    //   Self::each_addr(addr, Socket::listen, &stcp_listener); // .map() - what goes in map?
-    //   stcp_listener
-    // }
+    fn each_addr<A: ToSocketAddrs, F, T>(addr: A, mut f: F, mut S: &SmolTcpListener<'a>) -> Result<T, ListenError>
+    where
+        F: FnMut(IpListenEndpoint) -> Result<(), ListenError>,
+    {
+        let addrs = match addr.to_socket_addrs() {
+            Ok(addrs) => addrs,
+            Err(e) => return Err(e),
+        };
+        let mut last_err = None;
+        for addr in addrs {
+            match f(addr) {
+                Ok(l) => {
+                    // populate the local_addr of struct
+                    S.local_addr = addr;
+                    return Ok(l)
+                },
+                Err(e) => last_err = Some(e),
+            }
+        }
+        Err(last_err.unwrap_or_else(|| {
+            ListenError::Unaddressable // is this right? lol
+        }))
+    }
+    pub fn bind(addr: &dyn ToSocketAddrs<Iter=SocketAddr>) -> SmolTcpListener<'a> {
+      /*
+      passed to bind: 
+      "127.0.0.1:0"
+      SocketAddr::from(([127, 0, 0, 1], 443))
+      let addrs = [ SocketAddr::from(([127, 0, 0, 1], 80)),  SocketAddr::from(([127, 0, 0, 1], 443)), ];
+      */
+      // trial socket
+      // probably to change the buffers!!!!
+      let rx_buffer = SocketBuffer::new(vec![0; 64]);
+      let tx_buffer = SocketBuffer::new(vec![0; 64]);
+      let mut sock = Socket::new(rx_buffer, tx_buffer);
+      let mut stcp_listener = SmolTcpListener{local_addr: None, socket: sock};
+      Self::each_addr(addr, Socket::listen, &stcp_listener); // .map() - what goes in map?
+      stcp_listener
+    }
     
     // local_addr
     // return socketaddr from local_endpoint (stcp sock)
     // tuple contains a local endpoint and a remote endpoint and is set when connect() is called. 
     // listen_endpoint is what is passed into listen()
     // are they the same thing?
-    // pub fn local_addr(&self) -> Option<IpEndpoint> {
-    //     self.local_addr
-    // }
+    pub fn local_addr(&self) -> Option<IpEndpoint> {
+        self.local_addr
+    }
     
     // accept
     // create a smoltcpstream object
@@ -82,13 +82,13 @@ impl<'a> SmolTcpListener<'a> {
     // call connect() (i think?) on the copied socket to change it to stream state
     // transfer ownership of the socket from the listener to the stream object
     // return the stream object and socket address
-    // pub fn accept(&self) -> Result<(SmolTcpStream<'a>, IpListenEndpoint), ConnectError> {
-    //     // clone the listener
-    //     let mut clone = Self::try_clone();
-    //     let mut stream = SmolTcpStream {socket: clone};
-    //     clone.connect();
-    //     (stream, self.local_addr())
-    // }
+    pub fn accept(&self) -> Result<(SmolTcpStream<'a>, IpListenEndpoint), ConnectError> {
+        // clone the listener
+        let mut clone = Self::try_clone();
+        let mut stream = SmolTcpStream {socket: clone};
+        clone.connect();
+        (stream, self.local_addr())
+    }
 
 
     // try_clone
